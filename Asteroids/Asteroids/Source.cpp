f154002f199295cfbd8abe82ecd44f3100f953ce
ChrisMamatas <chrismamatas@yahoo.com>
2021-11-28 00:44:04 -0500

    added bubblesort

--- Asteroids/Asteroids/Source.cpp
+++ Asteroids/Asteroids/Source.cpp
@@ -1,8 +1,17 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
+
 	// Open a window
-	sf::RenderWindow window(sf::VideoMode(800, 600), "My Window");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Sorting Algorithm Visualized");
+
+	int arraySize = 50;
+	int* a = new int[arraySize];
+
+	srand(time(NULL));
+
+	for (int i = 0; i < arraySize; i++)
+		a[i] = rand() % 500 + 1;
 
 	// Run the program while the window is open
 	while (window.isOpen()) {
@@ -17,14 +26,30 @@ int main() {
 		// Clear the window to black
 		window.clear(sf::Color::Black);
 
-		sf::RectangleShape rect(sf::Vector2f(100.0f, 100.0f));
-		window.draw(rect);
-
+		// Draw the unsorted rectangles
+		for (int i = 0; i < arraySize; i++) {
+			sf::RectangleShape rect(sf::Vector2f(800 / arraySize, a[i]));
+			rect.setPosition(i * (800 / arraySize), 600 - a[i]);
+			window.draw(rect);
+		}
 		
+
+		// BubbleSort
+		for (int i = 0; i < arraySize - 1; i++) {
+			for (int j = 0; j < arraySize - i - 1; j++) {
+				if (a[j] > a[j + 1]) {
+					int temp = a[j];
+					a[j] = a[j + 1];
+					a[j + 1] = temp;
+				}
+			}
+		}
 
 
 		window.display();
+
 	}
 
+	delete[] a;
 	return 0;
 }
